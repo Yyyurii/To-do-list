f51af3f8166a01314facf9c4be2c281ed82eb2de
Yurii <yurii.kryvko@gmail.com>
2020-11-01 20:37:13 +0200

    checkbox

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,26 @@
-const post = (text) => {
-  const parent = document.querySelector('.posts-list__item');
-  const element = document.createElement('span');
+const post = (id, text) => {
+  const parent = document.querySelector('.posts-list');
+  const element = document.createElement('div');
+  element.classList.add('posts-list__item')
   element.innerText = text;
+  element.innerHTML = `
+    <div class="checkbox">
+      <input class="custom-checkbox" type="checkbox" id="${id}" value="indigo">
+      <label for="${id}">${text}</label>
+    </div>
+  `;
   parent.append(element);
 }
 
-post('hello');
+post('2','hello');
+post('3','work HARD!');
+
+const postList = document.querySelector('.posts-list');
+postList.addEventListener('click', (e) => {
+  const label = document.getElementsByTagName('label');
+  for (let i = 0; i < label.length; i++) {
+    if (e.target === label[i]) {
+      e.target.classList.toggle('cross-out');
+    }
+  }
+})
