f2ac2cb7171429f141748d9364332d2a51c68e89
Yurii <yurii.kryvko@gmail.com>
2020-11-10 17:40:55 +0200

    addEventListener 'keypress'

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 // для створення записів/завдання
 const postList = document.querySelector('.posts-list');
+const postInput = $('.post-input');
 
 const post = (text) => {
   const element = document.createElement('div');
@@ -28,33 +29,32 @@ postList.addEventListener('click', (e) => {
 })
 
 // btn 'add' анімація
-// при додаванні постів спрацьовує декілька раз, можливо помилка в цій функції
 $('.plus-btn').click(() => {
-  const postInput = $('.post-input');
   postInput.slideToggle('1000');
-  if ($('.post-input').css.display = 'inline-block') {
-    // console.log($('input:text').val());
-    $(this).keypress(function (e) {
-      const keycode = (e.keyCode ? e.keyCode : e.which);
-      if (keycode == '13') {
-        e.preventDefault();
-        const value = $('input:text').val();
-        post(value);
-        $('input:text').val('');
-        postInput.slideToggle('1000');
-      }
-    })
-  }
 })
+
+// onKeyPress 'enter' add task
+postInput.keypress(function (e) {
+  const keycode = (e.keyCode ? e.keyCode : e.which);
+  if (keycode == '13') {
+    e.preventDefault();
+    const value = $('input:text').val();
+    console.log(value);
+    post(value);
+    $('input:text').val('');
+    postInput.slideToggle('1000');
+  }
+});
 
 // Показує всі або виконані завдання
 const doneTaskBtn = document.querySelector('.header__done-posts');
 const allTaskBtn = document.querySelector('.header__all-posts');
 const header = document.querySelector('.header');
 const headerLineBottom = document.querySelector('.header__line-bottom');
-let postListItem = document.querySelectorAll('.posts-list__item');
+
 
 doneTaskBtn.addEventListener('click', () => {
+  let postListItem = document.querySelectorAll('.posts-list__item');
   postListItem.forEach(element => {
     if (element.classList.contains('done')) {
       element.style.display = 'block';
@@ -69,9 +69,10 @@ doneTaskBtn.addEventListener('click', () => {
 })
 
 allTaskBtn.addEventListener('click', () => {
+  let postListItem = document.querySelectorAll('.posts-list__item');
   postListItem.forEach(element => {
     element.style.display = 'block';
     header.style.cssText = 'background: linear-gradient(to right, white 50%, #8080802e 50%);';
-      headerLineBottom.style.cssText = 'right: 0;';
+    headerLineBottom.style.cssText = 'right: 0;';
   })
 })
