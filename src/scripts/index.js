9e5e5d62714e1d9c4e5da56595ef27cfc833f0ed
Yurii <yurii.kryvko@gmail.com>
2020-11-21 17:59:52 +0200

    deleted function letDownPost

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,44 +20,21 @@ const post = (text) => {
   postList.prepend(element);
 }
 
-post('hello');
-post('work HARD!');
+// відмітити як виконане зачеркнувши та перемістити в кінець списку
+function letDownPost() {
+  firstPostDone.parentNode.append(firstPostDone);
+};
 
-// відмітити як виконане зачеркнувши
 postList.addEventListener('click', (e) => {
-  let postListItem = document.querySelectorAll('.posts-list__item');
   const label = document.querySelectorAll('label');
   label.forEach(element => {
     if (e.target === element) {
       e.target.classList.toggle('cross-out');
       e.target.parentNode.classList.toggle('done');
-      e.target.parentNode.classList.toggle('undone');
     }
   });
-
-  postListItem.forEach(element => {
-    if (element.classList.contains('done')) {
-      element.parentNode.append(element);
-
-      // const el = new Promise((resolve, reject) => {
-      //   $(element).fadeOut('500');
-      //   console.log('fade in')
-      //   resolve();
-      // });
-      // el.then(() => {
-      //   setTimeout(() => {
-      //     element.parentNode.append(element);
-      //     console.log('prepend element')
-      //   }, 500)
-      // })
-      //   .then(() => {
-      //     setTimeout(() => {
-      //       $(element).fadeIn('500');
-      //     }, 500)
-      //   })
-    }
-  })
 })
+
 
 // btn 'add' анімація
 $('.plus-btn').click(() => {
@@ -70,9 +47,11 @@ postInput.keypress(function (e) {
   if (keycode == '13') {
     e.preventDefault();
     const value = $('input:text').val();
-    post(value);
-    $('input:text').val('');
-    postInput.slideToggle('1000');
+    if(value != '') {
+      post(value);
+      $('input:text').val('');
+      postInput.slideToggle('1000');
+    }
   }
 });
 
@@ -105,16 +84,6 @@ allTaskBtn.addEventListener('click', () => {
     header.style.cssText = 'background: linear-gradient(to right, white 50%, #8080802e 50%);';
     headerLineBottom.style.cssText = 'right: 0;';
   })
-})
-
-// extra menu
-postList.addEventListener('click', (e) => {
-  const extraMenuBtn = document.querySelectorAll('.extra-menu-btn');
-  extraMenuBtn.forEach(element => {
-    if (e.target === element) {
-      e.target.nextElementSibling.classList.toggle('extra-menu-display')
-    }
-  });
 })
 
 
